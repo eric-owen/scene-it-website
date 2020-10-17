da698f32b4f7fd42e7958c406ae8485e52c855c5
eric-owen <65836532+eric-owen@users.noreply.github.com>
2020-10-17 10:57:12 -0400

    test commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     function renderMovies(movieArray) {
-        const movieHTML = movieArray.map(function (currentMovie) {
+        let movieHTML = movieArray.map(function (currentMovie) {
 
             return `
             <div class="movie">
-                    <div class="test">
+                    <div class="movie-div">
 						<img src="${currentMovie.Poster}" alt="" width="200px" height="300px">
 						<p>
 							<span>${currentMovie.Title}</span>
 							<span>${currentMovie.Year}</span>
-						</p>
-                        <button>Add</button>
+                        </p>
+                        <button onclick="saveToWatchlist('${currentMovie.imdbID}')">Add</button>
+                        
                     </div>
 			</div>
             
@@ -19,12 +20,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
         return movieHTML.join('')
     }
+    // http://www.omdbapi.com/?i=tt3896198&apikey=cffaa38a
 
-
-    document.getElementById('search-form').addEventListener('submit', function (e) {
+    document.getElementById('search-form').addEventListener('submit', e => {
         e.preventDefault()
-        document.querySelector('.movies-container').innerHTML = renderMovies(movieData)
+        const moviesContainer = document.querySelector('.movies-container')
+        const searchText = e.target[0].value
+        let urlString = encodeURIComponent(searchText)
+        console.log(urlString)
+
+
+
+        axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=cffaa38a&s=' + urlString)
+            .then(resp => {
+                console.log(resp.data)
+                let movieHTML = renderMovies(resp.data.Search);
+                moviesContainer.innerHTML = movieHTML
+
+            })
+
     })
 
-
 })
+
+function saveToWatchlist(imdbID) {
+
+    let movie = movieData.find(function (currentMovie) {
+        return currentMovie.imdbID == imdbID;
+    })
+
+    let watchlistJSON = localStorage.getItem('watchlist')
+    let watchlist = JSON.parse(watchlistJSON)
+    if (watchlist === null) {
+        watchlist = []
+    }
+    watchlist.push(movie)
+    watchlistJSON = JSON.stringify(watchlist)
+    localStorage.setItem('watchlist', watchlistJSON)
+
+
+}
